c604661737904ef7aa7f3349bd92f174035a40c7
admin <admin@admin-PC>
2021-02-21 07:38:51 +0530

    Git commit

---
+++ src/test/java/Github/Gitprojects/Test1.java
@@ -0,0 +1,16 @@
+package Github.Gitprojects;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=5;
+		int b =10;
+		int sum = a+b;
+		
+		System.out.println("The sum value is :" +sum);
+	}
+
+}
